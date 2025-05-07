3b4f4adef447ea419177e79c74b3d259af9f6436
BorsanLeo <leonardo.borsan@gmail.com>
2025-05-07 03:35:23 +0200

    boh

---
+++ roman-number/src/test/java/it/unipd/mtss/AppTest.java
@@ -0,0 +1,26 @@
+////////////////////////////////////////////////////////////////////
+// NOME1 COGNOME1 MATRICOLA1 
+// NOME2 COGNOME2 MATRICOLA2 
+////////////////////////////////////////////////////////////////////
+package it.unipd.mtss;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+        //fail("Not yet implemented");
+    }
+}
